58b40af67acd6dcf40900f1944447e4c4fd1c4ed
Lucas Batista de Oliveira <lucasboliveira951@gmail.com>
2026-02-19 18:44:35 -0300

    Update page.js

--- app/(filme)/catalogo/page.js
+++ app/(filme)/catalogo/page.js
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
-import filmes from "@/app/data/filmes";
+import { filmes } from "@/app/data/filmes";
 
 const filmeLuz = filmes.find((f) => f.slug === "caminho-da-luz");
 
